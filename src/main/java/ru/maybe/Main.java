62dd01f18ecba7aebdcdb5cf46929ae7d8d49511
maybe78 <maybe78@gmail.com>
2018-11-16 13:03:33 +0300

    Inmemory H2 database & Spark web server Json REST Api Example.
    Initial commit.

--- src/main/java/ru/maybe/Main.java
+++ src/main/java/ru/maybe/Main.java
@@ -1,4 +1,4 @@
-package ru.sbrf.data;
+package ru.maybe;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
@@ -14,24 +14,21 @@ import java.util.*;
 import java.util.Date;
 
 
-public class Main {
-    private static final boolean DEBUG = true;
+class Main {
+    private static final boolean DEBUG = false;
     private static final Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-    private static final int CAFE_COUNT = 5;
-    private static final String CAFE_NAME_PREFIX = "Cafe";
-    private static final int DAILY_VISIT_GEN_LIMIT = 20;
-    private static final ThreadLocal<Integer> genPeriod = ThreadLocal.withInitial(() -> 1);
-    private static boolean fileDb = true;
-    private static Connection connection = null;
+    private static final ThreadLocal<Integer> dataGenThread = ThreadLocal.withInitial(() -> 1);
+
+    private Main() {
+    }
 
     public static void main(String[] args) {
         logger.setLevel(DEBUG ? Level.DEBUG : Level.INFO);
         H2DataBase.init();
 
-        TimerTask visitGenerator = new
-                DBDataGenerator();
+        TimerTask visitGenerator = new CafeDataGenerator();
         Timer t = new Timer(true);
-        t.scheduleAtFixedRate(visitGenerator, 5000, (long) genPeriod.get() * 1000);
+        t.scheduleAtFixedRate(visitGenerator, 5000, (long) dataGenThread.get() * 1000);
         logger.debug("Data generator started");
 
         RequestHandler.processRequests();
@@ -39,8 +36,10 @@ public class Main {
     }
 
     private static class H2DataBase {
+        private static boolean fileDb = true;
+        private static Connection connection = null;
         private static final String DB_NAME = "cafedb";
-        private static final String DB_CONNECTION_MEMORY = String.format("jdbc:h2:mem:%s", DB_NAME);
+        private static String dbConnection = String.format("jdbc:h2:mem:%s", DB_NAME);
         private static final String DB_CONNECTION_FILE = String.format("jdbc:h2:~/%s;AUTO_SERVER=true", DB_NAME);
         private static final String TABLE_NAME_COLUMN_HEADER = "TABLE_NAME";
 
@@ -52,16 +51,17 @@ public class Main {
                 Class.forName("org.h2.Driver");
                 if (fileDb) {
                     DeleteDbFiles.execute("~/", DB_NAME, false);
+                    dbConnection = DB_CONNECTION_FILE;
                 }
-                connection = DriverManager.getConnection(fileDb ? DB_CONNECTION_FILE : DB_CONNECTION_MEMORY);
+                connection = DriverManager.getConnection(dbConnection);
                 connection.setAutoCommit(true);
                 flushTables();
-                DBDataGenerator.createTables();
-                DBDataGenerator.populateCafeDataTable();
+                CafeDataGenerator.createTables();
+                CafeDataGenerator.populateCafeDataTable();
             } catch (SQLException e) {
                 logger.error("Initialization error", e);
             } catch (ClassNotFoundException e) {
-                logger.error("DB driver class not found.");
+                logger.error("DB driver class not found.", e);
             }
         }
 
@@ -74,7 +74,8 @@ public class Main {
             if (tables.size() <= 0) {
                 logger.debug("Setup tables for the new DataBase");
                 for (Object table : tables.values()) {
-                    sqlQueryExec(String.format("DROP TABLE %s; ", table));
+                    sqlQueryExec(String.format("drop table if exists %s;", table));
+                    logger.debug("Table {0} dropped.", table);
                 }
             }
         }
@@ -84,26 +85,56 @@ public class Main {
             Map<String, List<String>> table = new HashMap<>();
             try (Statement stmt = connection.createStatement()) {
                 if (columnsCount > 0) {
-                    try (
-                            ResultSet rs = stmt.executeQuery(sql)) {
+                    try (ResultSet rs = stmt.executeQuery(sql)) {
                         while (rs.next()) {
                             for (String c : returnedColumnsHeaders) {
-                                List<String> column = new ArrayList<>();
-                                if (table.containsKey(c)) {
-                                    column = table.get(c);
-                                }
+                                List<String> column = table.getOrDefault(c, new ArrayList<>());
                                 column.add(rs.getObject(c).toString());
                                 table.put(c, column);
                             }
                         }
-                        connection.commit();
                     }
+                } else {
+                    stmt.execute(sql);
                 }
+                connection.commit();
+                logger.info("SQL =>:\t{0}", sql);
             } catch (SQLException e) {
                 logger.error("Error executing the query: " + sql, e);
             }
-            if (DEBUG) logger.debug("SQL =>:\t{0}", sql);
             return table;
+        }
+    }
+
+    private static class CafeDataGenerator extends TimerTask {
+        private static final String CAFE_NAME_PREFIX = "Cafe";
+        private static final int CAFE_COUNT = 5;
+        private static final int DAILY_VISIT_GEN_LIMIT = 20;
+        private static int todayVisitGens = 1;
+        private static int currentCafe = 0;
+        private static int recordCount = 0;
+        private static List allCafeNames = Collections.emptyList();
+
+        private static void generateVisits() {
+            final Calendar date = Calendar.getInstance();
+            if (allCafeNames.isEmpty())
+                allCafeNames = (List) getCafeNames().get("NAME");
+            int randomVisitCount = new Random().nextInt(11);
+            CafeDataGenerator.insertRecord(recordCount, date.getTime(), allCafeNames.get(currentCafe).toString(), randomVisitCount);
+            currentCafe++;
+            todayVisitGens++;
+            recordCount++;
+            if (currentCafe >= CAFE_COUNT)
+                currentCafe = 0;
+            if (todayVisitGens >= DAILY_VISIT_GEN_LIMIT) {
+                date.add(Calendar.DATE, 1);
+                todayVisitGens = 1;
+            }
+        }
+
+        private static Map getCafeNames() {
+            return H2DataBase.sqlQueryExec("SELECT ID, NAME FROM CAFE_LIST GROUP BY ID;",
+                    "ID", "NAME");
         }
 
         private static void insertRecord(int id, Date date, String cafeName, int visits) {
@@ -111,29 +142,20 @@ public class Main {
             String dateDb = String.format("DATE\'%s\'", df.format(date));
             String sql = String.format(
                     "INSERT INTO VISIT_DATA VALUES (%d, \'%s\', %s, %d);", id, cafeName, dateDb, visits);
-            sqlQueryExec(sql);
+            H2DataBase.sqlQueryExec(sql);
         }
 
-        private static Map fetchCafeAverageVisits() {
-            return sqlQueryExec(
-                    "SELECT CAFE, " +
-                            "AVG(VISIT_COUNT) as AVERAGE " +
-                            "from VISIT_DATA " +
-                            "group by CAFE " +
-                            "order by CAFE;", "CAFE", "AVERAGE");
+        private static void populateCafeDataTable() {
+            for (int i = 1; i <= CAFE_COUNT; i++) {
+                H2DataBase.sqlQueryExec(String.format(
+                        "INSERT INTO CAFE_LIST VALUES (%d, \'%s\');", i, String.format("%s_%d", CAFE_NAME_PREFIX, i)));
+            }
         }
 
-        private static Map getCafeNames() {
-            return sqlQueryExec("SELECT ID, NAME FROM CAFE_LIST GROUP BY ID;",
-                    "ID", "NAME");
+        @Override
+        public void run() {
+            generateVisits();
         }
-    }
-
-    private static class DBDataGenerator extends TimerTask {
-        private final Calendar date = Calendar.getInstance();
-        private int todayVisitGens = 1;
-        private int currentCafe = 0;
-        private int recordCount = 0;
 
         private static void createTables() {
             H2DataBase.sqlQueryExec(
@@ -153,31 +175,7 @@ public class Main {
                             //" FOREIGN KEY (CAFE) REFERENCES CAFE_LIST(ID) "+
                             " );"
             );
-            logger.debug("DB Tables created.");
             H2DataBase.sqlQueryExec("SHOW TABLES;", H2DataBase.TABLE_NAME_COLUMN_HEADER);
-        }
-
-        private static void populateCafeDataTable() {
-            for (int i = 1; i <= CAFE_COUNT; i++) {
-                String sql = String.format(
-                        "INSERT INTO CAFE_LIST VALUES (%d, \'%s\');", i, String.format("%s_%d", CAFE_NAME_PREFIX, i));
-                H2DataBase.sqlQueryExec(sql);
-            }
-        }
-
-        @Override
-        public void run() {
-            int randomVisitCount = new Random().nextInt(11);
-            List allCafeNames = (List) H2DataBase.getCafeNames().get("NAME");
-            H2DataBase.insertRecord(recordCount, date.getTime(), allCafeNames.get(currentCafe).toString(), randomVisitCount);
-            currentCafe++;
-            todayVisitGens++;
-            recordCount++;
-            if (currentCafe >= CAFE_COUNT) currentCafe = 0;
-            if (todayVisitGens >= DAILY_VISIT_GEN_LIMIT) {
-                date.add(Calendar.DATE, 1);
-                todayVisitGens = 1;
-            }
         }
     }
 
@@ -187,9 +185,18 @@ public class Main {
 
         private static void processRequests() {
             Gson gson = new Gson();
-            Spark.get("/average", (request, response) -> H2DataBase.fetchCafeAverageVisits(), gson::toJson);
+            Spark.get("/average", (request, response) -> getAverageVisits(), gson::toJson);
+        }
+
+        private static Map getAverageVisits() {
+            return H2DataBase.sqlQueryExec(
+                    "SELECT CAFE, " +
+                            "AVG(VISIT_COUNT) as AVERAGE " +
+                            "from VISIT_DATA " +
+                            "group by CAFE " +
+                            "order by CAFE;", "CAFE", "AVERAGE"
+            );
         }
     }
 
 }
-
